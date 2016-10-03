ed779468759f152f9ecaffbb90dd8df8f796ee12
stephen-sun7 <stgg159@163.com>
2016-10-03 20:20:58 +0800

    first commit

--- 2016-spring/task3-5/src/constants/CalendarActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const SELECT = "SELECT";
-export const SLIDE = "SLIDE";
-export const ZOOM = "ZOOM";
--- 2016-spring/task3-5/src/constants/CalendarDirectionTypes.js
+++
@@ -1,4 +0,0 @@
-export const LEFT = "LEFT";
-export const RIGHT = "RIGHT";
-export const IN = "IN";
-export const OUT = "OUT";
